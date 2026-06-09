b9214ee9cc54281a22a3ea3fe201bbc5628aeab9
Pavan021 <pavanpanni008@gmail.com>
2026-06-10 01:46:41 +0530

    Completed ECommerce

--- src/main/java/com/ecommerce/project/models/Category.java
+++ src/main/java/com/ecommerce/project/models/Category.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
     @Id
@@ -13,29 +19,29 @@ public class Category {
     private Long categoryId;
     private String categoryName;
 
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
+//    public Category(Long categoryId, String categoryName) {
+//        this.categoryId = categoryId;
+//        this.categoryName = categoryName;
+//    }
 
-    public Category()   // default constructor
-    {
+//    public Category()   // default constructor
+//    {
+//
+//    }
 
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
+//    public Long getCategoryId() {
+//        return categoryId;
+//    }
+//
+//    public void setCategoryId(long categoryId) {
+//        this.categoryId = categoryId;
+//    }
+//
+//    public String getCategoryName() {
+//        return categoryName;
+//    }
+//
+//    public void setCategoryName(String categoryName) {
+//        this.categoryName = categoryName;
+//    }
 }
